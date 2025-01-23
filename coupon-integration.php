4e3593eb911469326e1be21769f3f7cfd2576423
glmfrk <golamfaruk204@gmail.com>
2025-01-23 13:10:23 +0600

    first commit

---
+++ coupon-integration.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Plugin Name: Coupon Integration
+ * Description: Handles coupon codes, subscriber ID validation, and API communication between two WordPress sites.
+ * Version: 1.0
+ * Author: Gulam Faruk
+ */
